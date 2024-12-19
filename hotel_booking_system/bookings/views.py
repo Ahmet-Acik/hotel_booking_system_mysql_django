8687762fd4b9155b919d3b8a1327e56f0af78aa7
drahmetacik <a.acik@icloud.com>
2024-12-19 12:18:19 +0000

    feat: add CRUD views for guest management

--- hotel_booking_system/bookings/views.py
+++ hotel_booking_system/bookings/views.py
@@ -1,18 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Guest, Room, Booking, Payment, Service
+from .forms import GuestForm, RoomForm, BookingForm, PaymentForm, ServiceForm
 
-# Create your views here.
-# filepath: /Users/drahmetacik/Projects/hotel_bs_mysql_django/hotel_booking_system/bookings/views.py
-from django.shortcuts import render
-from .models import Guest, Room, Booking
-
+# Guest Views
 def guest_list(request):
     guests = Guest.objects.all()
     return render(request, 'bookings/guest_list.html', {'guests': guests})
 
-def room_list(request):
-    rooms = Room.objects.all()
-    return render(request, 'bookings/room_list.html', {'rooms': rooms})
+def guest_create(request):
+    if request.method == 'POST':
+        form = GuestForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('guest_list')
+    else:
+        form = GuestForm()
+    return render(request, 'bookings/guest_form.html', {'form': form})
 
-def booking_list(request):
-    bookings = Booking.objects.all()
-    return render(request, 'bookings/booking_list.html', {'bookings': bookings})
+def guest_update(request, pk):
+    guest = get_object_or_404(Guest, pk=pk)
+    if request.method == 'POST':
+        form = GuestForm(request.POST, instance=guest)
+        if form.is_valid():
+            form.save()
+            return redirect('guest_list')
+    else:
+        form = GuestForm(instance=guest)
+    return render(request, 'bookings/guest_form.html', {'form': form})
+
+def guest_delete(request, pk):
+    guest = get_object_or_404(Guest, pk=pk)
+    if request.method == 'POST':
+        guest.delete()
+        return redirect('guest_list')
+    return render(request, 'bookings/guest_confirm_delete.html', {'guest': guest})
+
+# Repeat similar views for Room, Booking, Payment, and Service
